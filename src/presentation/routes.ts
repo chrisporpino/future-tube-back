3ec0e5fa177ca2d0ea2bd798f20c0096839bb5b2
Christhopher Porpino <cgporpinof@gmail.com>
2020-05-16 15:35:33 -0300

    criei deleteVideoEndpoint

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -5,6 +5,7 @@ import { loginEndpoint } from './endpoints/user/loginEndpoint'
 import signupEndpoint from './endpoints/user/signupEndpoint'
 import changePasswordEndpoint from './endpoints/user/changePasswordEndpoint'
 import uploadVideoEndpoint from './endpoints/videos/uploadVideoEndpoint'
+import deleteVideoEndpoint from './endpoints/videos/deleteVideoEndpoint'
 
 const app = express()
 const cors = require("cors")
@@ -21,6 +22,7 @@ app.post("/user/change-password", changePasswordEndpoint)
 app.post("/videos/upload", uploadVideoEndpoint)
 app.get("/videos/all", getAllVideosEndpoint)
 app.get("/videos/details", getVideoDetailsEndpoint)
+app.delete('/videos/:videoId', deleteVideoEndpoint)
 
 
 export default app
